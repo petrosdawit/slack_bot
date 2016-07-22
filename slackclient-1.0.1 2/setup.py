155f44f087bc8d7af632c04d33b770bae9427e63
petrosdawit <petros_dawit@brown.edu>
2016-07-22 15:49:39 -0400

    hi

---
+++ slackclient-1.0.1 2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='slackclient',
+      version='1.0.1',
+      description='Python client for Slack.com',
+      url='http://github.com/slackhq/python-slackclient',
+      author='Ryan Huber',
+      author_email='ryan@slack-corp.com',
+      license='MIT',
+      packages=['slackclient'],
+      install_requires=[
+        'websocket-client',
+        'requests',
+      ],
+      zip_safe=False)
