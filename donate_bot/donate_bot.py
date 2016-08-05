302ae763da53b4cd0a8238084e0c669d7e4e5151
petrosdawit <petros_dawit@brown.edu>
2016-08-05 11:20:28 -0400

    updated some code, look at update.md august 4

--- donate_bot/donate_bot.py
+++ donate_bot/donate_bot.py
@@ -5,12 +5,12 @@ from slackclient import SlackClient
 import re
 
 """ 
-    StarterBot Class. It will account for the responses and be the main class 
+    DonateBot Class. It will account for the responses and be the main class 
     Use this link to find api methods https://api.slack.com/methods
     ex of api call for sending message: self._slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)  
 """
 
-class StarterBot(object):
+class DonateBot(object):
 
     def __init__(self, slack_client, BOT_ID):
         """
@@ -39,6 +39,7 @@ class StarterBot(object):
 
         self._org = {}
         self._dollar_amount = {}
+        self._name = {}
 
         #self._bot_responses is dictonary where the values will be arrays containing all the possible responses the bot can say lowercased for each user
         self._bot_responses = {}
@@ -51,10 +52,12 @@ class StarterBot(object):
         self._BOT_ID = BOT_ID
         #user and channel which are currently set to None when you start this class
         self._user = None
-        self._channel = None        
+        self._channel = None 
+        #users will be an array keeping track of each of the ids in the team
+        self._users = []       
 
     """ 
-        Setters and getters for the slack_client, channel, command and old_command(previous command said to the starterbot)
+        Setters and getters for the slack_client, channel, command and old_command(previous command said to the donatebot)
     """    
 
     def get_slack_client(self):
@@ -66,14 +69,17 @@ class StarterBot(object):
     def set_channel(self, channel):
         self._channel = channel 
 
+    def get_self_users(self):
+        return self._users
+
     def get_users(self):
         #calls the slack_client's api call to list out info of the users. You can get the names of the users
         users_list = self._slack_client.api_call("users.list")
-        users = [] 
         for x in users_list["members"]:
             #get ids of users
-            users.append(x["id"])
-        for user in users:
+            self._users.append(x["id"])
+            self._name[x["id"]] = x["name"]
+        for user in self._users:
             #create initial holders for the user's information that needs to be recorded
             self._mistakes[user] = 0
             self._attempting_to_donate[user] = False
@@ -88,9 +94,32 @@ class StarterBot(object):
             "canceling donation. thank you for your time.", "you have made too many mistakes. i'm cancelling the donation",
             "please respond with either yes to confirm the donation or no to cancel it.", "sup...", "hello. nice to meet you!",
             "the available commands are \n*commands* : to list all the possible commands \n*donate* : to start donating to your favorite charities\n      _ex: donate to " + 
-            "'name of your charity' $20.00_ \n*hello*, *hi*, *hey*, *sup* : to say hi to the bot!", "not sure what you mean. message starterbot by typing 'commands' " + 
+            "'name of your charity' $20.00_ \n*hello*, *hi*, *hey*, *sup* : to say hi to the bot!", "not sure what you mean. message donatebot by typing 'commands' " + 
             "to find all the available commands", "please make sure to include a dollar amount (ex: 25 = $25.00) and a partnered organization (ex: unicef) to make " + 
-            "the donation"]                 
+            "the donation"]
+
+    def updateUsers(self):
+        users_list = self._slack_client.api_call("users.list")  
+        for x in users_list["members"]:
+            if x["id"] not in self._users:
+                self._users.append(x["id"])
+                self._mistakes[x["id"]] = 0
+                self._attempting_to_donate[x["id"]] = False
+                self._first_donate_message_occured[x["id"]] = False
+                self._response[x["id"]] = ""
+                self._old_response[x["id"]] = None
+                self._old_command[x["id"]] = ""
+                self._command[x["id"]] = ""
+                self._org[x["id"]] = ""
+                self._name[x["id"]] = x["name"]
+                self._dollar_amount[x["id"]] = ""   
+                self._bot_responses[x["id"]] = ["would you like to donate $" + self._dollar_amount[x["id"]].lower() + " to " + self._org[x["id"]].lower() + ", respond with yes or no", "thanks for donating to " + self._org[x["id"]].lower(),
+                "canceling donation. thank you for your time.", "you have made too many mistakes. i'm cancelling the donation",
+                "please respond with either yes to confirm the donation or no to cancel it.", "sup...", "hello. nice to meet you!",
+                "the available commands are \n*commands* : to list all the possible commands \n*donate* : to start donating to your favorite charities\n      _ex: donate to " + 
+                "'name of your charity' $20.00_ \n*hello*, *hi*, *hey*, *sup* : to say hi to the bot!", "not sure what you mean. message donate by typing 'commands' " + 
+                "to find all the available commands", "please make sure to include a dollar amount (ex: 25 = $25.00) and a partnered organization (ex: unicef) to make " + 
+                "the donation"]                                     
 
     def handle_command(self, command, channel, user):
         """
@@ -169,15 +198,16 @@ class StarterBot(object):
         elif command[:8].startswith("commands") and command[:9].strip() == command[:8]:
             self._response[user] = "The available commands are \n*commands* : to list all the possible commands \n*donate* : to start donating to your favorite charities\n      _ex: donate to 'name of your charity' $20.00_ \n*hello*, *hi*, *hey*, *sup* : to say hi to the bot!"                
         else :
-            self._response[user] = "Not sure what you mean. Message starterbot by typing 'commands' to find all the available commands"
+            self._response[user] = "Not sure what you mean. Message donatebot by typing 'commands' to find all the available commands"
             self._attempting_to_donate[user] = False
             self._first_donate_message_occured[user] = False   
 
-
-
         #posts message 
-        if user != os.environ.get("BOT_ID"):             
+        if channel.startswith("D"):            
             self._slack_client.api_call("chat.postMessage", channel=channel, text=self._response[user], as_user=True)
+        else:
+            self._response[user] = "<@" + self._name[user] + ">:" + self._response[user]
+            self._slack_client.api_call("chat.postMessage", channel=channel, text=self._response[user], as_user=True)            
 
     def get_channel_user_from_rtm(self, slack_rtm_output):
         """
@@ -216,7 +246,7 @@ class StarterBot(object):
             and will return None, if our prev command and our current command are the same. However, we need a second check b/c
             with only if self._command != self._old_command:, the response will be repeated twice. for ex: user says 'hi'. the current
             command is 'hi' and the prev command is none. so the bot replies. now the new command is 'the reply' and the prev command is 'hi'
-            the slack_client will read the starterbot's responses as well. in order to fix this, there is a second check where we check
+            the slack_client will read the donatebot's responses as well. in order to fix this, there is a second check where we check
             if the new command is any of standard bot responses and if it is, we set the new command and the old command to None
         """
         output_list = slack_rtm_output
@@ -234,25 +264,34 @@ if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 0.25 # 0.25 second delay between reading from firehose
     slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
     BOT_ID = "<@" + os.environ.get("BOT_ID") + ">:"
-    starter_bot = StarterBot(slack_client, BOT_ID)
-    if starter_bot.get_slack_client().rtm_connect():
-        starter_bot.get_users()
-        print("StarterBot connected and running!")
+    #donate_bot
+    donate_bot = DonateBot(slack_client, BOT_ID)
+    if donate_bot.get_slack_client().rtm_connect():
+        donate_bot.get_users()
+        count = 0
+        print("DonateBot connected and running!")
         while True:
-            output_list = starter_bot.get_slack_client().rtm_read()
-            channel, user = starter_bot.get_channel_user_from_rtm(output_list)
-            starter_bot.set_channel(channel)
+            output_list = donate_bot.get_slack_client().rtm_read()
+            channel, user = donate_bot.get_channel_user_from_rtm(output_list)
+            donate_bot.set_channel(channel)
             command = None
-            if channel != None and user != None:
+            if channel != None and user != None and user in donate_bot.get_self_users():
                 #dm channel ids start with 'D', so if the channel starts with D, then its a dm. we use the sending_direct_messages(output_list)
                 #for that instead. channel messages start with 'C'.
                 if channel.startswith('D'):
-                    command = starter_bot.sending_direct_messages(output_list, user)
+                    command = donate_bot.sending_direct_messages(output_list, user)
                 else: 
-                    command = starter_bot.sending_message_in_channels(output_list)
+                    command = donate_bot.sending_message_in_channels(output_list)
             if command and channel:
-                starter_bot.handle_command(command, channel, user)
-            time.sleep(READ_WEBSOCKET_DELAY)   
+                donate_bot.handle_command(command, channel, user)
+            time.sleep(READ_WEBSOCKET_DELAY)
+            #update users every 100/4 = 25s. 4 because the READ_WEBSOCKET_DELAY is 0.25s so 4 to make 1 second    
+            if count % 100 == 0:
+                donate_bot.updateUsers()
+            count += 1
+            #reset the counter
+            if count == 1000:
+                count = 0
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
 
